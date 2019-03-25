88928aa333efdea478d550d8277157e6760829a9
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-03-25 17:19:39 +0200

    When deleting a parent template, assign one of the child templates as the new parent

--- src/Models/TemplateModel.php
+++ src/Models/TemplateModel.php
@@ -8,7 +8,32 @@ class TemplateModel extends Model
 {
     protected $table = 'nova_page_manager';
 
+    protected $fillable = ['parent_id'];
+
     protected $casts = [
         'data' => 'object'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($template) {
+            // Is a parent template
+            if ($template->parent_id === null) {
+                // Find child templates
+                $childTemplates = TemplateModel::where('parent_id', '=', $template->id)->get();
+                if (count($childTemplates) === 0) return;
+
+                // Pick the first template randomly and let it become the parent
+                $childTemplates[0]->update(['parent_id' => null]);
+                $newParentId = $childTemplates[0]->id;
+
+                // Update others
+                for ($i = 1; $i < count($childTemplates); $i++) {
+                    $childTemplates[$i]->update(['parent_id' => $newParentId]);
+                }
+            }
+        });
+    }
 }
