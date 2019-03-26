48b9e74dbd3492b5e8935d8745faf722b3b8f05e
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-03-26 15:25:21 +0200

    Switch sidebar icon

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,6 +1,6 @@
 <h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="sidebar-icon">
-        <path fill="var(--sidebar-icon)" class="heroicon-ui" d="M20 6a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h7.41l2 2H20zM4 6v12h16V8h-7.41l-2-2H4z"/>
+    <svg viewBox="0 0 24 24" class="sidebar-icon">
+        <path fill="var(--sidebar-icon)" fill="#000000" d="M16,15H9V13H16V15M19,11H9V9H19V11M19,7H9V5H19V7M3,5V21H19V23H3A2,2 0 0,1 1,21V5H3M21,1A2,2 0 0,1 23,3V17C23,18.11 22.11,19 21,19H7A2,2 0 0,1 5,17V3C5,1.89 5.89,1 7,1H21M7,3V17H21V3H7Z" />
     </svg>
     <span class="sidebar-label">
         Page manager
@@ -9,23 +9,29 @@
 
 <ul class="list-reset mb-8">
     <li class="leading-wide mb-4 text-sm">
-        <router-link :to="{
-            name: 'index',
-            params: {
-                resourceName: 'pages'
-            }
-        }" class="text-white ml-8 no-underline dim">
+        <router-link
+            :to="{
+                name: 'index',
+                params: {
+                    resourceName: 'pages'
+                }
+            }"
+            class="text-white ml-8 no-underline dim"
+        >
             Pages
         </router-link>
     </li>
 
     <li class="leading-wide mb-4 text-sm">
-        <router-link :to="{
-            name: 'index',
-            params: {
-                resourceName: 'regions'
-            }
-        }" class="text-white ml-8 no-underline dim">
+        <router-link
+            :to="{
+                name: 'index',
+                params: {
+                    resourceName: 'regions'
+                }
+            }"
+            class="text-white ml-8 no-underline dim"
+        >
             Regions
         </router-link>
     </li>
