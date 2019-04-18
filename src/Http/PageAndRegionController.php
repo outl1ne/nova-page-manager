8f8e0b88d7c6707546c9742c5a8a4341c6a8d2bd
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-04-18 11:01:21 +0300

    TemplateResolver no longer necessary

--- src/Http/PageAndRegionController.php
+++ src/Http/PageAndRegionController.php
@@ -6,8 +6,6 @@ use Illuminate\Routing\Controller;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use OptimistDigital\NovaPageManager\Models\Page;
 use OptimistDigital\NovaPageManager\Models\Region;
-use OptimistDigital\NovaPageManager\Nova\TemplateResource;
-use OptimistDigital\NovaPageManager\Nova\Page as PageResource;
 
 class PageAndRegionController extends Controller
 {
