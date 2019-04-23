5c88bfeffd7a84a75897452f483fc2a2a3976863
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-04-23 15:09:56 +0300

    Initial version of nova_get_pages() helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,35 @@
 <?php
+
 use OptimistDigital\NovaPageManager\Models\Page;
+use Illuminate\Support\Collection;
 
 if (!function_exists('nova_get_pages')) {
     function nova_get_pages()
     {
+        $formatPages = function (Collection $pages) use (&$formatPages) {
+            $data = [];
+            $pages->each(function ($page) use (&$data, &$formatPages) {
+                $localeChildren = Page::where('locale_parent_id', $page->id)->get();
+                $_pages = collect([$page, $localeChildren])->flatten();
+                $_data = [
+                    'locales' => $_pages->pluck('locale'),
+                    'id' => $_pages->pluck('id', 'locale'),
+                    'name' => $_pages->pluck('name', 'locale'),
+                    'slug' => $_pages->pluck('slug', 'locale'),
+                    'template' => $page->template,
+                ];
+
+                $children = Page::where('parent_id', $page->id)->get();
+                if ($children->count() > 0) {
+                    $_data['children'] = $formatPages($children);
+                }
+
+                $data[] = $_data;
+            });
+            return $data;
+        };
+
         $parentPages = Page::whereNull('parent_id')->whereNull('locale_parent_id')->get();
-        // dd($parentPages);
-        return $parentPages;
+        return $formatPages($parentPages);
     }
 }
