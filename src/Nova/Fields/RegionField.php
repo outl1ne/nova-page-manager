2b7216c702a25c60ff42b43688094bfbe9001565
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-04-25 11:57:08 +0300

    Fix RegionField validation

--- src/Nova/Fields/RegionField.php
+++ src/Nova/Fields/RegionField.php
@@ -13,10 +13,13 @@ class RegionField extends Field
     public function __construct($name, $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, 'template', $resolveCallback);
+    }
 
-        $regions = array_map(function ($template) {
-            return $template::$name;
-        }, NovaPageManager::getRegionTemplates());
+    public function resolve($resource, $attribute = null)
+    {
+        parent::resolve($resource, $attribute);
+
+        $regions = $this->getAvailableRegions($resource);
 
         $this->withMeta([
             'asHtml' => true,
@@ -24,6 +27,24 @@ class RegionField extends Field
             'existingRegions' => Region::whereNull('locale_parent_id')->get()->pluck('template', 'id'),
         ]);
 
-        $this->rules('required', 'in:' . implode(',', $regions));
+        $regionsTableName = NovaPageManager::getRegionsTableName();
+        $locale = request()->get('locale');
+        $this->rules('required', "unique:$regionsTableName,template,{{resourceId}},id,locale,$locale");
+    }
+
+    public function getAvailableRegions(Region $region = null): array
+    {
+        if (isset($region) && isset($region->id) && isset($region->template)) {
+            return [$region->template];
+        }
+
+        return collect(NovaPageManager::getRegionTemplates())
+            ->filter(function ($template) {
+                return !Region::where('template', $template::$name)->exists();
+            })
+            ->map(function ($template) {
+                return $template::$name;
+            })
+            ->toArray();
     }
 }
