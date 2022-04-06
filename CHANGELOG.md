2310f9115fdae999d2d391f5b9a296a0993a9934
Kaspar Rosin <kaspar.rosin@gmail.com>
2022-04-06 16:22:10 +0300

    fix an issue with get_page_by_template helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.0.5] - 2021-12-14
+
+### Fixed
+
+- Fixed an issue with `nova_page_manager_get_page_by_template` where it would return a page that was not locale parent.
+
 ## [4.0.4] - 2021-12-14
 
 ### Fixed
--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Collection;
-use OptimistDigital\NovaPageManager\Models\TemplateModel;
-use OptimistDigital\NovaPageManager\NovaPageManager;
 use Laravel\Nova\Panel;
 use Laravel\Nova\Fields\Heading;
+use Illuminate\Support\Collection;
+use OptimistDigital\NovaPageManager\NovaPageManager;
+use OptimistDigital\NovaPageManager\Models\TemplateModel;
 
 // ------------------------------
 // nova_get_pages_structure
@@ -406,13 +406,12 @@ if (!function_exists('nova_page_manager_get_page_by_template')) {
     {
         if (empty($template)) return [];
 
-        $pageQuery = NovaPageManager::getPageModel()::where('template', $template);
+        $pageQuery = NovaPageManager::getPageModel()::where('template', $template)->whereNull('locale_parent_id');
         if (!empty($locale)) $pageQuery->where('locale', $locale);
         if (!empty($previewToken)) $pageQuery->where('preview_token', $previewToken);
 
         $page = $pageQuery->first();
         if (!$page) return [];
-
         if (!empty($locale)) return nova_format_page($page);
 
         $localeChildren = NovaPageManager::getPageModel()::where('template', $template)->where('locale_parent_id', $page->id)->get();
