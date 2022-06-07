0c908ece75e946c6411181abc160820b1903b39a
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2022-06-07 18:25:38 +0300

    WIP docs

--- src/Stubs/PageTemplate.php
+++ src/Stubs/PageTemplate.php
@@ -7,20 +7,24 @@ use Outl1ne\PageManager\Template;
 
 class :className extends Template
 {
+    // Name displayed in CMS
     public function name(Request $request) {
         return static::class;
     }
 
+    // Fields displayed in CMS
     public function fields(Request $request): array
     {
         return [];
     }
 
-    public function metaData(Request $request): array
-    {
-        return [];
+    // Resolve data for serialization
+    public function resolve(Request $request, $page): array {
+        // Modify data as you please (ie turn ID-s into models)
+        return $page->data;
     }
 
+    // Optional suffix to the route (ie {blogPostName})
     public function pathSuffix(Request $request) {
         return null;
     }
--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,44 @@
 <?php
 
-// TODO
+use Outl1ne\PageManager\NPM;
+
+if (!function_exists('npmGetPageByPath')) {
+    function npmGetPageByPath($path)
+    {
+        // TODO Find page by path and return data
+    }
+}
+
+if (!function_exists('npmFormatPage')) {
+    function npmFormatPage($page)
+    {
+        if (empty($page)) return null;
+
+        $template = NPM::getPageTemplateBySlug($page->template);
+        if (empty($template)) return null;
+
+        $request = request();
+
+        $pageData = [
+            'id' => $page->id,
+            'created_at' => $page->created_at,
+            'updated_at' => $page->updated_at,
+            'name' => $page->name ?: [],
+            'slug' => $page->slug ?: [],
+            'path' => $page->path ?: [],
+            'parent_id' => $page->parent_id,
+            'data' => $template->resolve($request, $page),
+            'template' => $page->template ?: null,
+        ];
+
+        // if ($template::$seo) {
+        //     $pageData['seo'] = [
+        //         'title' => $page->seo_title,
+        //         'description' => $page->seo_description,
+        //         'image' => $page->seo_image,
+        //     ];
+        // }
+
+        return $pageData;
+    }
+}
