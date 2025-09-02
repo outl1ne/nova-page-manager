6ebda6cdfe636fe5f7e958034af1bb12c68dd919
Kaspar Rosin <kaspar.rosin@gmail.com>
2025-09-02 11:53:09 +0300

    wip

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -19,6 +19,8 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->publishes([
+            __DIR__.'/../../config/nova-page-manager.php' => config_path('nova-page-manager.php'),
+        ]);
     }
 }
